a714a1e46fdc4a15e34602228f78873df20b7403
Andres Arango <ararango@comcast.net>
2022-03-14 16:19:04 -0400

    changes made on call

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ var footer = document.createElement('footer');
 // ======= Create event handler functions =======
 // render tag list
 var renderTaglist = function() {
+  aside.replaceChildren();
   // make title
   var tagTitle = document.createElement('h3');
   tagTitle.setAttribute('class', 'tag-title');
@@ -33,20 +34,51 @@ var renderTaglist = function() {
     tagLi.setAttribute('class', 'tag');
     tagLi.textContent = tag;
     tagUl.appendChild(tagLi);
+    tagLi.addEventListener('click', function() {
+      renderFeed(tagLi.textContent);
+    });
   });
   // append to aside
   aside.appendChild(tagTitle);
   aside.appendChild(tagUl);
+
+
 };
 
-var addQuestion = function () {
+var postQuestion = function () {
+  debugger;
   var text = document.getElementById('message').value;
   console.log(text);
-  questionArray.push(text);
+  questionArray.unshift(text);
   renderFeed();
-}
+
+  // if text contains a hashtag
+  var idx = text.indexOf('#');
+  if (idx !== -1) {
+    // hashtag function as a function of text
+    tagFinder(text.substring(idx, text.length));
+  }
+};
+
+var tagFinder = function(text) {
+  var spaceIndex = text.indexOf(' ');
+
+  if (spaceIndex !== -1) {
+    var tag = text.substring(0, spaceIndex);
+  } else {
+    var tag = text;
+  }
+
+  if (!tags.includes(tag)) {
+    tags.unshift(tag);
+  }
+
+  renderTaglist();
+};
+
 var renderFeed = function (tag) {
-  var keyword = tag || ' ';
+  questionList.replaceChildren();
+  var keyword = tag || '';
   questionArray.forEach( element => {
     if (element.includes(keyword)) {
       var question = document.createElement('li');
@@ -54,8 +86,8 @@ var renderFeed = function (tag) {
       question.textContent = element;
       questionList.appendChild(question);
     }
-  })
-}
+  });
+};
 
 // ======= Set attributes / event listeners (providing appropriate handlers as input) =======
 
@@ -65,7 +97,10 @@ header.setAttribute('class', 'header');
 aside.setAttribute('class', 'tag-list');
 main.setAttribute('class', 'main');
 draftPost.setAttribute('class', 'draft-post');
-draftPost.setAttribute('onsubmit', 'addQuestion()');
+draftPost.addEventListener('submit', function(e) {
+  e.preventDefault();
+  postQuestion();
+});
 feed.setAttribute('class', 'feed');
 postTextInput.setAttribute('type', 'text');
 postTextInput.setAttribute('placeholder', 'Ask us anything!');
@@ -76,6 +111,7 @@ submitBtn.textContent = 'Submit your silly question';
 submitBtn.setAttribute('class', 'submit-btn');
 submitBtn.setAttribute('type', 'submit');
 questionList.setAttribute('class', 'question-list');
+
 
 //  ======= Append new HTML elements to the DOM =======
 header.appendChild(title);
